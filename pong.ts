55e43874d989cf158fbc369148652e444faf0b22
Timothy Cool <tcoolpg@gmail.com>
2023-06-19 20:20:27 -0700

    feature: update pong game with collision

--- pong.ts
+++ pong.ts
@@ -1,3 +1,4 @@
+import { Vector3 } from 'three';
 import Zylem from './src/index';
 import { PerspectiveType } from './src/interfaces/Perspective';
 
@@ -6,12 +7,17 @@ const { Box, Sphere } = Zylem.GameEntityType;
 const game = Zylem.create({
 	id: 'pong',
 	perspective: PerspectiveType.Fixed2D,
+	globals: {
+		p1Score: 0,
+		p2Score: 0,
+	},
 	stage: {
 		children: () => {
 			return [
 				{
 					name: 'paddle1',
 					type: Box,
+					shape: new Vector3(2, 10, 1),
 					setup: (entity) => {
 						entity.setPosition(20, 0, 0);
 					},
@@ -28,6 +34,7 @@ const game = Zylem.create({
 				{
 					name: 'paddle2',
 					type: Box,
+					shape: new Vector3(2, 10, 1),
 					setup: (entity) => {
 						entity.setPosition(-20, 0, 0);
 					},
@@ -44,12 +51,30 @@ const game = Zylem.create({
 				{
 					name: 'ball',
 					type: Sphere,
+					shape: new Vector3(1, 1, 1),
+					props: {
+						direction: 1
+					},
 					setup(entity) {
 						entity.setPosition(0, 0, 0);
 					},
 					update(delta, { entity, inputs }) {
-						entity.moveX(delta);
-					}
+						const { direction } = entity.getProps();
+						if (direction === 1) {
+							entity.moveX(0.5);
+						} else if (direction === -1) {
+							entity.moveX(-0.5);
+						}
+					},
+					collision: (entity, other) => {
+						console.log(other.name);
+						if (other.name === 'paddle1') {
+							entity._props.direction = -1;
+						} else if (other.name === 'paddle2') {
+							entity._props.direction = 1;
+						}
+					},
+					destroy: () => { }
 				}
 			];
 		},
