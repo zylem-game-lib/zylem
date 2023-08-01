c8d633f16f2c3ad0a0b85f3df72b3095113ea187
Timothy Cool <tcoolpg@gmail.com>
2023-07-31 21:24:26 -0700

    chore: cleanup dead code

--- src/game/ZylemGame.ts
+++ src/game/ZylemGame.ts
@@ -5,30 +5,6 @@ import { Clock } from 'three';
 import { GameOptions } from '../interfaces/Game';
 import { PerspectiveType } from "../interfaces/Perspective";
 
-function Game({ app, stages = [] }: { app: HTMLElement | string, stages?: Function[] }) {
-	return (target: any) => {
-		const gameInstance = new target();
-		// const pyramidInstance = new PyramidGame({
-		// 	loop: gameInstance.loop.bind(gameInstance),
-		// 	setup: gameInstance.setup.bind(gameInstance),
-		// 	globals: Globals.getInstance(),
-		// 	stages: stages,
-		// });
-		// pyramidInstance.ready.then(() => {
-		// 	let appElement;
-		// 	if (typeof app === 'string') {
-		// 		appElement = document.querySelector<HTMLDivElement>(app)!;
-		// 	} else {
-		// 		appElement = app;
-		// 	}
-		// 	appElement.appendChild(pyramidInstance.domElement());
-		// 	if (gameInstance.ready) {
-		// 		gameInstance.ready.bind(gameInstance)();
-		// 	}
-		// })
-	}
-}
-
 // We should have an abstraction for entering, exiting, and updating.
 // Zylem Game should only require stages, global state, and game loop.
 
@@ -47,33 +23,9 @@ export class ZylemGame implements GameOptions {
 		this.gamePad = new GamePad();
 		this.clock = new Clock();
 		this.blueprintOptions = options;
-		// this.pause = false;
-		// this._loop = loop;
-		// this._setup = setup;
-		// this._globals = globals;
-		// this._stages = stages;
-		// this.ready = new Promise(async (resolve, reject) => {
-		// 	try {
-		// 		const world = await this.loadPhysics();
-
-		// 		for (const stageCreator of this._stages) {
-		// 			const stage = stageCreator.prototype.init(world) as PyramidStage;
-		// 			this.stages.push(stage);
-		// 		}
-		// 		await this.gameSetup();
-		// 		const self = this;
-		// 		requestAnimationFrame(() => {
-		// 			self.gameLoop(self)
-		// 		});
-		// 		resolve(true);
-		// 	} catch (error) {
-		// 		reject(error);
-		// 	}
-		// });
 		this.createCanvas();
 		this.loadStage(options);
 		this.currentStage = this.id;
-		// this.createStage();
 	}
 
 	async loadStage(options: GameOptions) {
@@ -91,22 +43,7 @@ export class ZylemGame implements GameOptions {
 	async gameLoop() {
 		const inputs = this.gamePad.getInputs();
 		const ticks = this.clock.getDelta();
-		// if (!this.pause) {
-		// 	this.stage().update({
-		// 		delta: ticks,
-		// 		inputs
-		// 	});
-		// }
 
-		// this._loop({
-		// 	ticks,
-		// 	inputs,
-		// 	stage: this.stage(),
-		// 	globals: this._globals,
-		// 	game: this,
-		// });
-
-		// this.stage().render();
 		const stage = this.stages[this.currentStage];
 		const options = {
 			inputs,
@@ -122,18 +59,6 @@ export class ZylemGame implements GameOptions {
 
 	start() {
 		this.gameLoop();
-	}
-
-	async gameSetup() {
-		// const commands = await Create(this.stage());
-		// const gameSetupParameters = {
-		// 	create: commands.create,
-		// 	globals: this._globals,
-		// 	camera: this.stage()._camera
-		// };
-
-		// this.stage()._setup(gameSetupParameters);
-		// this._setup(gameSetupParameters);
 	}
 
 	createStage(id: string) {
@@ -165,4 +90,4 @@ export class ZylemGame implements GameOptions {
 	}
 }
 
-export default Game;
+export default ZylemGame;
