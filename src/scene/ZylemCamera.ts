171c290c35a74e1b01d0708164402d5651b57b13
Timothy Cool <tcoolpg@gmail.com>
2023-08-05 23:16:02 -0700

    feature: create camera based on perspective game option

--- src/scene/ZylemCamera.ts
+++ src/scene/ZylemCamera.ts
@@ -1,4 +1,6 @@
 import { Vector2, Camera, PerspectiveCamera, Vector3, Object3D, OrthographicCamera } from 'three';
+import { GameState } from '@state/index';
+import { PerspectiveType } from '@interfaces/Perspective';
 
 export class ZylemCamera {
 	cameraRig: Object3D;
@@ -7,15 +9,69 @@ export class ZylemCamera {
 
 	constructor(screenResolution: Vector2) {
 		let aspectRatio = screenResolution.x / screenResolution.y;
-		this.camera = new PerspectiveCamera(45, aspectRatio, 0.1, 1000);
-		// this.camera = new OrthographicCamera();
-		this.camera.position.z = 20;
-		this.camera.position.y = 6 * Math.tan(Math.PI / 3);
-		// this.follow = null;
+
+		const z = 25;
+		const position = new Vector3(0, 0, z);
+		const gamePerspective = GameState.state.perspective;
+		this.camera = this[gamePerspective](aspectRatio, position);
 		this.cameraRig = new Object3D();
-		this.cameraRig.position.set(0, 0, -50);
+		this.cameraRig.position.set(0, 0, z);
 		this.cameraRig.add(this.camera);
 		this.camera.lookAt(new Vector3(0, 0, 0));
+	}
+
+	[PerspectiveType.ThirdPerson](aspectRatio: number): Camera {
+		console.warn('Third person camera not fully implemented');
+		return new PerspectiveCamera(45, aspectRatio, 0.1, 1000);
+	}
+
+	[PerspectiveType.Fixed2D](aspectRatio: number, position: Vector3): Camera {
+		const frustumSize = position.z;
+		const distance = position.distanceTo(new Vector3(0, 0, 0));
+		const orthographicCamera = new OrthographicCamera(
+			frustumSize * aspectRatio / -2,
+			frustumSize * aspectRatio / 2,
+			frustumSize / 2,
+			frustumSize / -2,
+			0.1,
+			distance * 2
+		);
+
+		orthographicCamera.position.copy(position);
+		return orthographicCamera;
+	}
+
+	[PerspectiveType.FirstPerson](): Camera {
+		console.warn('First person camera not fully implemented');
+		return new PerspectiveCamera(45, 1, 0.1, 1000);;
+	}
+
+	[PerspectiveType.Flat2D](aspectRatio: number, position: Vector3): Camera {
+		console.warn('Flat2D camera not fully implemented');
+		return this[PerspectiveType.Fixed2D](aspectRatio, position);
+	}
+
+	[PerspectiveType.Isometric](aspectRatio: number, position: Vector3): Camera {
+		console.warn('Isometric camera not fully implemented');
+		const frustumSize = 20;
+		const isometricCamera = new OrthographicCamera(
+			frustumSize * aspectRatio / -2,
+			frustumSize * aspectRatio / 2,
+			frustumSize / 2,
+			frustumSize / -2,
+			0.1,
+			position.z * 2
+		);
+		isometricCamera.position.copy(position);
+		isometricCamera.lookAt(new Vector3(0, 0, 0));
+
+		isometricCamera.rotation.set(
+			Math.atan(-1 / Math.sqrt(2)),
+			0,
+			0,
+			'YXZ'
+		);
+		return isometricCamera;
 	}
 
 	update() {
