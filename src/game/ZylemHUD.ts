7bc0c3979ca035feb8d2317306a03745a491ece3
Timothy Cool <tcoolpg@gmail.com>
2023-09-17 17:37:27 -0700

    feature: basic HUD with text binding to globals

---
+++ src/game/ZylemHUD.ts
@@ -0,0 +1,51 @@
+import { Vector3 } from 'three';
+import { GameState } from '@state/index';
+import SpriteText from 'three-spritetext';
+
+export interface HUDTextOptions {
+	text: string;
+	binding: string;
+	position: Vector3;
+}
+
+export interface HUDText {
+	sprite: SpriteText;
+	binding: string;
+	position: Vector3;
+}
+
+export class ZylemHUD {
+	_hudText: HUDText[];
+
+	constructor() {
+		this._hudText = [];
+	}
+
+	createText({ text, binding, position }: HUDTextOptions) {
+		const spriteText = new SpriteText(text);
+		spriteText.textHeight = 2;
+		const hudText = {
+			sprite: spriteText,
+			binding,
+			position
+		}
+		this._hudText.push(hudText);
+	}
+
+	update() {
+		if (!this._hudText) {
+			return;
+		}
+		this._hudText.forEach(hud => {
+			const { binding } = hud;
+			if (!binding) {
+				return;
+			}
+			const globals = GameState.state.globals;
+			const value = `${globals[binding]}`;
+			if (value) {
+				hud.sprite.text = value;
+			}
+		})
+	}
+}
