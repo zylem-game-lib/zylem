1e8bca1493602ed2361c2da7db802594d2dc58e0
Timothy Cool <tcoolpg@gmail.com>
2024-01-06 23:53:05 -0800

    feat: use sprite animations in example platformer game

--- examples/platformer/player.ts
+++ examples/platformer/player.ts
@@ -17,14 +17,38 @@ export function Player() {
 			name: 'idle',
 			file: 'platformer/idle.png'
 		}, {
+			name: 'idle-left',
+			file: 'platformer/idle-left.png'
+		}, {
 			name: 'run-1',
 			file: 'platformer/run-1.png'
 		}, {
 			name: 'run-2',
 			file: 'platformer/run-2.png'
 		}, {
+			name: 'run-1-left',
+			file: 'platformer/run-1-left.png'
+		}, {
+			name: 'run-2-left',
+			file: 'platformer/run-2-left.png'
+		}, {
 			name: 'jump',
 			file: 'platformer/jump.png'
+		}, {
+			name: 'jump-left',
+			file: 'platformer/jump-left.png'
+		}
+		],
+		animations: [{
+			name: 'run',
+			frames: ['run-1', 'idle', 'run-2'],
+			speed: 0.1,
+			loop: true,
+		}, {
+			name: 'run-left',
+			frames: ['run-1-left', 'idle-left', 'run-2-left'],
+			speed: 0.1,
+			loop: true,
 		}],
 		size: new Vector3(2, 2, 1),
 		props: {
@@ -33,23 +57,33 @@ export function Player() {
 			jumpTimer: 0,
 			jumpVelocity: 16,
 			gravity: -9.8,
-			onGround: false
+			onGround: false,
+			movingLeft: false,
 		},
 		setup: (entity: any) => {
 			entity.setPosition(0, 0, 0);
 		},
 		update: (_delta: number, { entity: player, inputs }: any) => {
-			const { moveLeft, moveRight, buttonA } = inputs[0];
+			const { moveLeft, moveRight, buttonA, buttonB } = inputs[0];
 			let { x: velX, y: velY } = player.getVelocity();
 
 			if (!player.onGround) {
 				velY += player.gravity * _delta;
 			}
 
+			if (player.jumpState === JumpState.Idle) {
+				const useIdleSprite = player.movingLeft ? 'idle-left' : 'idle';
+				player.setSprite(useIdleSprite);
+			}
+
 			if (moveLeft) {
 				velX = -10;
+				player.movingLeft = true;
+				player.setAnimation('run-left', _delta);
 			} else if (moveRight) {
 				velX = 10;
+				player.movingLeft = false;
+				player.setAnimation('run', _delta);
 			} else {
 				velX = 0;
 			}
@@ -67,11 +101,8 @@ export function Player() {
 			}
 
 			if (player.jumpState === JumpState.Jumping || player.jumpState === JumpState.Falling) {
-				player.setSprite('jump');
-			}
-
-			if (player.jumpState === JumpState.Idle) {
-				player.setSprite('idle');
+				const useJumpSprite = player.movingLeft ? 'jump-left' : 'jump';
+				player.setSprite(useJumpSprite);
 			}
 
 			if (player.jumpTimer > player.jumpTime || !buttonA) {
