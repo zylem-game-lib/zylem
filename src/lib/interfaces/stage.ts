dc9749f029139e74bb7722217baffab47a85d2b7
Timothy Cool <tcoolpg@gmail.com>
2024-06-08 23:02:33 -0700

    chore: move stage blueprint to separate file

---
+++ src/lib/interfaces/stage.ts
@@ -0,0 +1,16 @@
+import { Color, Vector3 } from "three";
+import { PerspectiveType } from "./perspective";
+import { Conditions, GameBlueprint, SetupCallback } from "./game";
+import { UpdateFunction } from "./entity";
+
+export interface StageBlueprint {
+	id?: string;
+	gravity?: Vector3;
+	perspective: PerspectiveType;
+	backgroundImage?: string;
+	backgroundColor: Color | number;
+	setup: SetupCallback;
+	children: (globals?: any) => any[];
+	conditions: Array<Conditions<GameBlueprint["globals"]>>;
+	update?: UpdateFunction<any> | null;
+}
