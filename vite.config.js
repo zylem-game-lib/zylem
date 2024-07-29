55d4e265d4d748caca0857ee07ee954cdacf0e20
Timothy Cool <tcoolpg@gmail.com>
2024-07-28 20:50:06 -0700

    feature: update vite config to support glsl

--- vite.config.js
+++ vite.config.js
@@ -1,65 +1,66 @@
-import typescript from '@rollup/plugin-typescript';
 import path from 'path';
-import { typescriptPaths } from 'rollup-plugin-typescript-paths';
-import { visualizer } from 'rollup-plugin-visualizer';
-import progress from 'vite-plugin-progress';
-import colors from 'picocolors';
-
 import { defineConfig } from 'vite';
 
+import typescript from '@rollup/plugin-typescript';
+import { typescriptPaths } from 'rollup-plugin-typescript-paths';
+import { visualizer } from 'rollup-plugin-visualizer';
+
+import viteProgressBar from 'vite-plugin-progress';
+import colors from 'picocolors';
+
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [
-		// https://github.com/jeddygong/vite-plugin-progress
-		progress({
-			format: `Building ${colors.green('[:bar]')} :percent :eta`,
-			total: 100,
-			width: 60,
-			// complete: "=",
-			// incomplete: "",
-		}),
-	],
-	resolve: {
-		alias: [
-			{
-				find: '~',
-				replacement: path.resolve(__dirname, './src'),
-			},
+export default defineConfig(async () => {
+	const glsl = (await import('vite-plugin-glsl')).default;
+
+	return {
+		plugins: [
+			// https://github.com/jeddygong/vite-plugin-progress
+			viteProgressBar({
+				format: `Building ${colors.green('[:bar]')} :percent :eta`,
+				total: 100,
+				width: 60,
+			}),
+			glsl()
 		],
-	},
-	server: {
-		port: 3123,
-	},
-	// preview: {
-	//   port: 3124,
-	// },
-	// https://vitejs.dev/guide/build.html#library-mode
-	build: {
-		manifest: true,
-		minify: true,
-		reportCompressedSize: true,
-		lib: {
-			entry: path.resolve(__dirname, 'src/main.ts'),
-			fileName: 'main',
-			formats: ['es', 'cjs'],
-		},
-		rollupOptions: {
-			external: [],
-			plugins: [
-				typescriptPaths({
-					preserveExtensions: true,
-				}),
-				typescript({
-					sourceMap: false,
-					declaration: true,
-					outDir: 'dist',
-					exclude: ['**/__tests__'],
-				}),
-				visualizer({
-					title: 'visualizer - zylem',
-					template: 'network',
-				}),
+		resolve: {
+			alias: [
+				{
+					find: '~',
+					replacement: path.resolve(__dirname, './src'),
+				},
 			],
 		},
-	},
+		server: {
+			port: 3123,
+		},
+		// https://vitejs.dev/guide/build.html#library-mode
+		build: {
+			manifest: true,
+			minify: true,
+			reportCompressedSize: true,
+			lib: {
+				entry: path.resolve(__dirname, 'src/main.ts'),
+				fileName: 'main',
+				formats: ['es', 'cjs'],
+			},
+			rollupOptions: {
+				external: [],
+				plugins: [
+					typescriptPaths({
+						preserveExtensions: true,
+					}),
+					typescript({
+						sourceMap: false,
+						declaration: true,
+						outDir: 'dist',
+						exclude: ['**/__tests__'],
+					}),
+					visualizer({
+						title: 'visualizer - zylem',
+						template: 'network',
+					}),
+				],
+			},
+		},
+	}
 });
