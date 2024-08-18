62765cc92f63ab3c8200fbb3097d356a18d2e3b2
Timothy Cool <tcoolpg@gmail.com>
2024-08-18 16:39:48 -0700

    feature: add simple example of moving sphere

---
+++ examples/playground/simple.ts
@@ -0,0 +1,13 @@
+import { game, stage, sphere } from '../../src/main';
+
+const example = game(
+	stage(),
+	sphere({
+		update: ({ entity: ball, inputs }) => {
+			const { horizontal, vertical } = inputs[0];
+			ball.moveXY(horizontal * 5, -vertical * 5);
+		}
+	})
+);
+
+example.start();
