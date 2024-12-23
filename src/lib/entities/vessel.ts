2a1d8319bde7b30fa0974817b99b0a93b7a2f3ee
Timothy Cool <tcoolpg@gmail.com>
2024-12-22 19:03:24 -0800

    refactor: wip vessel updates and box builder

--- src/lib/entities/vessel.ts
+++ src/lib/entities/vessel.ts
@@ -2,29 +2,66 @@ import { DestroyFunction } from "../core/destroy";
 import { SetupFunction } from "../core/setup";
 import { UpdateFunction } from "../core/update";
 
-export interface ChildNode<U = object> {
+abstract class Vessel {
+	protected parent: Vessel | null = null;
+	protected children: Vessel[] = [];
+
+	public setParent(parent: Vessel | null): void {
+		this.parent = parent;
+	}
+
+	public getParent(): Vessel | null {
+		return this.parent;
+	}
+
+	public add(vessel: Vessel): void {
+		this.children.push(vessel);
+		vessel.setParent(this);
+	}
+
+	public remove(vessel: Vessel): void {
+		const index = this.children.indexOf(vessel);
+		if (index !== -1) {
+			this.children.splice(index, 1);
+			vessel.setParent(null);
+		}
+	}
+
+	public isComposite(): boolean {
+		return true;
+	}
+
+	public abstract operation(): string;
+
+	public setup(): void {
+		this.children.forEach(child => child.setup());
+	}
+
+	public update(): void {
+		this.children.forEach(child => child.update());
+	}
+
+	public destroy(): void {
+		this.children.forEach(child => child.destroy());
+	}
+}
+
+
+export interface ChildVessel<U = object> {
 	options?: U;
-	children?: ChildNode<U>[];
+	children?: ChildVessel<U>[];
 	update?: (params: any) => void;
 	setup?: (params: any) => void;
 	destroy?: () => void;
 }
 
-export interface Node<T extends ChildNode<U> = any, U = object> {
-	options?: U;
-	children?: T[];
-	update?: (params: any) => void;
-	setup?: (params: any) => void;
-	destroy?: () => void;
-}
-
-export interface NodeFunction<T extends Node<U>, U extends ChildNode<object> = object> {
+export interface VesselFunction<T extends Vessel<U>, U extends ChildVessel<object> = object> {
 	(): T;
 	(options: U): T;
-	(...children: ChildNode<U>[]): Node<U>;
+	(...children: ChildVessel<U>[]): Vessel<U>;
 }
 
-export function vessel<T extends Node>(...args: Array<T | Partial<T['options']>>): T {
+export function vessel<T extends Vessel>(...args: Array<T | Partial<T['options']>>): T {
 	const vessel: T = {
 		children: [],
 		update: () => { },
@@ -34,8 +71,7 @@ export function vessel<T extends Node>(...args: Array<T | Partial<T['options']>>
 
 	for (const arg of args) {
 		if ('children' in arg) {
-			vessel.children = vessel.children || [];
-			vessel.children.push(arg as T);
+			vessel.add(arg as T);
 		} else {
 			vessel.options = { ...vessel.options, ...(arg as Partial<T['options']>) };
 		}
@@ -44,7 +80,7 @@ export function vessel<T extends Node>(...args: Array<T | Partial<T['options']>>
 	return vessel;
 }
 
-export class ZylemVessel {
+export class ZylemVessel extends Vessel {
 	nodes: ZylemVessel[] = [];
 	setup: SetupFunction<ZylemVessel> = () => { };
 	update: UpdateFunction<ZylemVessel> = () => { };
