85ab7cb17f5a51986e010df39bd1b1bc509a3c08
Timothy Cool <tcoolpg@gmail.com>
2025-04-15 16:04:06 -0700

    chore: create separate tests for camera and entity

--- examples/playground/04-vessel-test.ts
+++ examples/playground/04-vessel-test.ts
@@ -1,30 +1,14 @@
-import { game, vessel, box, plane, stage } from '../../src/main';
-import { Vector2, Vector3 } from 'three';
+import { game, vessel, stage } from '../../src/main';
 
-const grassPath = 'playground/grass.jpg';
-// const test = vessel();
-// const test2 = vessel(vessel(), vessel());
-// const test3 = vessel({ test: 'test' }, vessel({ test1: 'test' }), vessel({ test2: 'test' }));
-const myBox = await box({
-	size: new Vector3(4, 2, 1),
-	position: { x: 0, y: 3, z: 0 },
-	collision: { static: false },
-	material: { path: grassPath, repeat: new Vector2(2, 2) },
-});
-const myPlane = await plane({
-	tile: new Vector2(200, 200),
-	position: { x: 0, y: 0, z: 0 },
-	collision: { static: true },
-	material: { path: grassPath, repeat: new Vector2(20, 20) },
-});
+const test = vessel();
+const test2 = vessel(vessel(), vessel());
+const test3 = vessel({ test: 'test' }, vessel({ test1: 'test' }), vessel({ test2: 'test' }));
 
-const stage1 = stage({ gravity: new Vector3(0, -9.82, 0) });
+const stage1 = stage();
 const testGame = game(
 	{ id: 'zylem', debug: true },
 	stage1,
-	// test, test2, test3,
-	myBox,
-	myPlane
+	test, test2, test3,
 );
 
 testGame.start();
