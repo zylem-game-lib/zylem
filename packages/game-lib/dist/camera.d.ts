653e8a7143920eadb6879b247d8d4e0988a48818
Timothy Cool <tcoolpg@gmail.com>
2026-01-26 21:33:05 -0800

    refinement: planet demo and build dist removal

--- packages/game-lib/dist/camera.d.ts
+++
@@ -1,6 +0,0 @@
-export { P as PerspectiveType, d as Perspectives, c as createCamera } from './camera-DrTbdaJ1.js';
-import 'three';
-import 'three/webgpu';
-import 'three/examples/jsm/postprocessing/EffectComposer.js';
-import './entity-Bq_eNEDI.js';
-import '@dimforge/rapier3d-compat';
